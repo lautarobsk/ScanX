f4f0196b248cd5617c56354543caadcc5f61037c
lautaro arce <lautaroarce345@gmail.com>
2026-06-25 10:41:26 -0300

    upd 3

--- apps/tracker/admin.py
+++ apps/tracker/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Vehiculo, Camara, TramoCalle, RegistroDeteccion
 
-# Register your models here.
+@admin.register(Vehiculo)
+class VehiculoAdmin(admin.ModelAdmin):
+    list_display = ('patente', 'marca', 'modelo', 'sospechoso')
+    search_fields = ('patente',)
+    list_filter = ('sospechoso',)
+
+@admin.register(Camara)
+class CamaraAdmin(admin.ModelAdmin):
+    # Actualizado con 'direccion' y 'activa'
+    list_display = ('codigo_identificador', 'direccion', 'activa')
+    search_fields = ('codigo_identificador',)
+    list_filter = ('activa',)
+
+@admin.register(TramoCalle)
+class TramoCalleAdmin(admin.ModelAdmin):
+    # Actualizado con 'distancia'
+    list_display = ('origen', 'destino', 'nombre_calle', 'distancia')
+
+@admin.register(RegistroDeteccion)
+class RegistroDeteccionAdmin(admin.ModelAdmin):
+    list_display = ('vehiculo', 'camara', 'timestamp', 'fecha_registro_sistema')
+    list_filter = ('camara', 'timestamp')
+    date_hierarchy = 'timestamp'
--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tracker/', include('tracker.urls')),
+    path('api/tracker/', include('apps.tracker.urls')),
 ]
